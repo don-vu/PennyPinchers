1ee6f9397eaa7ed4bb4560de64f28259d49bbd6b
Min K <73949340+CMK62@users.noreply.github.com>
2023-01-07 16:36:46 -0700

    Moved Signup and Login page

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -16,18 +16,6 @@ function Home() {
     <div>
       <h1>Home</h1>
       <p>Welcome to the home page, PENNY PINCHER!</p>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form.Group>
-      </Form>
-      <button>Don't Have an account? Sign Up here!</button>
 
     </div>
   );
