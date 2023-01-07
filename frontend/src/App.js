afe43928a799324af35224e09ef9eeffc914162d
Min K <73949340+CMK62@users.noreply.github.com>
2023-01-07 14:38:48 -0700

    Title Names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,10 +10,10 @@ function App() {
     <div className="container">
       <Router>
         <div className="navbar">
-          <Link to="/">home</Link>
-          <Link to="/login">login</Link>
-          <Link to="/page2">page2</Link>
-          <Link to="/page3">page3</Link>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/page2">Sign Up</Link>
+          <Link to="/page3">Calculator</Link>
         </div>
 
         <Routes>
