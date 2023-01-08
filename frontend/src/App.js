1d7ca974e9eba30b545845dedb4cddc1e5d57cbd
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 17:46:07 -0700

    Add page 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/home";
 import Page1 from "./pages/page1/page1";
 import Page2 from "./pages/page2/page2";
 import Page3 from "./pages/page3/page3";
+import Page4 from "./pages/page4/page4";
 import { Signup } from "./Signup";
 import React, { useState } from "react";
 
@@ -21,6 +22,7 @@ function App() {
           <Link to="/login">Login</Link>
           <Link to="/page2">Sign Up</Link>
           <Link to="/page3">Calculator</Link>
+          <Link to="/page4">Page 4</Link>
         </div>
 
         <Routes>
@@ -28,6 +30,7 @@ function App() {
           <Route path="/login" element={<Page1 />} />
           <Route path="/page2" element={<Page2 />} />
           <Route path="/page3" element={<Page3 />} />
+          <Route path="/page4" element={<Page4 />} />
         </Routes>
       </Router>
     </div>
---
+++ frontend/src/pages/page4/page4.js
@@ -0,0 +1,9 @@
+function page4() {
+  return (
+    <div>
+      <h1>Page 4</h1>
+    </div>
+  );
+}
+
+export default page4;
