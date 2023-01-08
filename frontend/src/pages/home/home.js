00c1836879ac4ea8837730302b905f02803cdf6b
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 20:24:20 -0700

    Update home

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import "./home.css";
 
 function Home() {
   const { user, setUser } = useState(null);
@@ -10,13 +10,15 @@ function Home() {
 
   return (
 
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page, PENNY PINCHER!</p>
-      <p>Being a student is stressful enough, let us relieve some of the stress by helping you organize your funds.
-        Based on your selected criteria, we will help you generate a customized plan so you can keep track
-        of how much you spend for each category every month for the length of your academic term</p>
-    </div>
+<div className='containerHome'>
+      <div>
+        <h1>Home</h1>
+        <p>Welcome to the home page, PENNY PINCHER!</p>
+        <p>Being a student is stressful enough, let us relieve some of the stress by helping you organize your funds.
+          Based on your selected criteria, we will help you generate a customized plan so you can keep track
+          of how much you spend for each category every month for the length of your academic term</p>
+      </div>
+</div>
   );
 }
 
