3f110d7b6298e3adcbbccc122a068b693d3bd0bc
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 18:46:56 -0700

    Implement POST with database

--- backend/server.js
+++ backend/server.js
@@ -3,35 +3,45 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const url =
-  "mongodb+srv://demo:admin@cluster0.hlvpvjl.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://demo:admin@cluster0.hlvpvjl.mongodb.net/pennypinchers?retryWrites=true&w=majority";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
 
-const people = [];
 
 const personSchema = new mongoose.Schema({
+  id: Number,
   userName: String,
   password: String,
 });
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model("Person", personSchema, "users");
+// app.get("/api/persons", (request, response) => {
+//   response.json(people[0]);
+// });
 
-mongoose.connect(url).then((result) => {
-  console.log("connected to MongoDB");
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  mongoose.connect(url).then((result) => {
+    console.log("connected to MongoDB");
+  
+    const person = new Person({
+      // random number between 1 and 1000
+      id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
+      userName: `${body.userName}`,
+      password: `${body.password}`,
+    });
+  
+    return person.save();
+  }).then(() => {
+    console.log("person saved!");
+    return mongoose.connection.close();
+  })
+  response.json("ok");
+});
 
-  const person = new Person({
-    userName: "Mark",
-    password: "pennyeater",
-  });
-
-  return person.save();
-}).then(() => {
-  console.log("person saved!");
-  return mongoose.connection.close();
-})
 
 const PORT = process.env.PORT || "8080";
 app.listen(PORT, () => {
--- frontend/src/pages/page2/page2.js
+++ frontend/src/pages/page2/page2.js
@@ -1,18 +1,29 @@
+import axios from "axios";
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function Page1() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.userName.value, event.target.password.value);
+    const person = {
+      userName: event.target.userName.value,
+      password: event.target.password.value,
+    };
+    axios.post("http://localhost:8080/api/persons", person)
+  };
+
   return (
 
     <div>
       <h1>SignUp </h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email"  name="userName"/>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
+          <Form.Control type="password" placeholder="Enter password" name="password"/>
           <Button variant="primary" type="submit">
             Submit
           </Button>
