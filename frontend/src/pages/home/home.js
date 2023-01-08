c553d53cca815a75c6d04fc5c51a3a06f8af4c59
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 20:15:34 -0700

    Update home

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-
+import "./home.css";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function Home() {
-  const { user, setUser } = useState(null)
-
+  const { user, setUser } = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-  }
+  };
 
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page, PENNY PINCHER!</p>
-      <p>Being a student is stressful enough, let us relieve some of the stress by helping you organize your funds. 
-        Based on your selected criteria, we will help you generate a customized plan so you can keep track 
-        of how much you spend for each category every month for the length of your academic term</p>
+    <div className="containerHome">
+      <div className="borderHome">
+        <h1>Home</h1>
+        <p>Welcome to the home page, PENNY PINCHER!</p>
+        <p>
+          Being a student is stressful enough, let us relieve some of the stress
+          by helping you organize your funds. Based on your selected criteria,
+          we will help you generate a customized plan so you can keep track of
+          how much you spend for each category every month for the length of
+          your academic term
+        </p>
+      </div>
     </div>
   );
 }
