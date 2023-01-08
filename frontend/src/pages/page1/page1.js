a059384bd1bff684fad8ceb95784e6eb5c145d3d
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 19:24:59 -0700

    Style log-in page

--- frontend/src/pages/page1/page1.js
+++ frontend/src/pages/page1/page1.js
@@ -1,27 +1,33 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import "./page1.css";
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function Page1() {
-  return (
+  const navigate = useNavigate();
 
-    <div>
-      <h1>Login </h1>
+  const handleClick = (event) => {
+    navigate('/page2');
+  };
+
+  return (
+    <div className="form">
       <Form>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="formBasicEmail" className="border">
+          <h1>Login </h1>
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Enter password" />
-          <Button variant="primary" type="submit">
+          <Button className="submit" variant="primary" type="submit">
             Submit
           </Button>
         </Form.Group>
+          <button className="redirect" onClick={handleClick}>Don't Have an account? Sign Up here!</button>
       </Form>
-      <button>Don't Have an account? Sign Up here!</button>
     </div>
   );
-
 }
 
 export default Page1;
