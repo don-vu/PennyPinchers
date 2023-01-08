064fd4cdb1121bdab380d6f6a40470d57b9f95d4
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 19:03:01 -0700

    Clean up sign up page

--- frontend/src/pages/page2/page2.js
+++ frontend/src/pages/page2/page2.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import './page2.css';
 
 function Page1() {
   const handleSubmit = (event) => {
@@ -16,15 +17,14 @@ function Page1() {
 
   return (
 
-    <div>
-      <h1>SignUp </h1>
+    <div className="form">
       <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="formBasicEmail" className="border">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email"  name="userName"/>
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Enter password" name="password"/>
-          <Button variant="primary" type="submit">
+          <Button className="submit" variant="primary" type="submit">
             Submit
           </Button>
         </Form.Group>
