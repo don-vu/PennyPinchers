4eb37cf7bda70843c90caf7371f50c9d6a9e02ec
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-07 20:59:47 -0700

    Update pie chart

--- frontend/src/pages/page4/page4.js
+++ frontend/src/pages/page4/page4.js
@@ -1,38 +1,17 @@
 import { PieChart } from "react-minimal-pie-chart";
-import './page4.css'
+import "./page4.css";
 
 function page4() {
   const data = [
-    {
-      name: "Category 1",
-      value: 25,
-    },
-    {
-      name: "Category 2",
-      value: 15,
-    },
-    {
-      name: "Category 3",
-      value: 35,
-    },
-    {
-      name: "Category 4",
-      value: 25,
-    },
+    { title: "One", value: 10, color: "#E38627", label: "10%" },
+    { title: "Two", value: 15, color: "#C13C37", label: "15%" },
+    { title: "Three", value: 20, color: "#6A2135", label: "20%" },
   ];
 
   return (
     <div>
       <h1>Dashboard</h1>
-      <PieChart className="piechart"
-
-        data={[
-          { title: "One", value: 10, color: "#E38627" },
-          { title: "Two", value: 15, color: "#C13C37" },
-          { title: "Three", value: 20, color: "#6A2135" },
-        ]}
-      />
-      ;
+      <PieChart className="piechart" data={data} />;
     </div>
   );
 }
