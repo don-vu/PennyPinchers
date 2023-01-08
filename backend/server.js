90b4e62739f9db2b4683d367269c0cb40e89ef30
Don Vu <33820836+Don-Vu@users.noreply.github.com>
2023-01-08 00:47:23 -0700

    Change server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const { response } = require("express");
 
 const url =
   "mongodb+srv://demo:admin@cluster0.hlvpvjl.mongodb.net/pennypinchers?retryWrites=true&w=majority";
@@ -9,6 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
+
+const assets = [
+]
 
 
 const personSchema = new mongoose.Schema({
@@ -46,25 +50,41 @@ app.post("/api/persons", (request, response) => {
 });
 
 
+// app.post("/api/assets", (request, response) => {
+//   const body = request.body;
+//   mongoose.connect(url).then((result) => {
+//     console.log("connected to MongoDB");
+  
+//     const asset = new Asset({
+//       // random number between 1 and 1000
+//       id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
+//       asset: `${body.asset}`,
+//       amount: `${body.amount}`,
+//     }
+//     )
+  
+//     return asset.save();
+//   }).then(() => {
+//     console.log("asset saved!");
+//     return mongoose.connection.close();
+//   })
+//   response.json("ok");
+// });
 app.post("/api/assets", (request, response) => {
   const body = request.body;
-  mongoose.connect(url).then((result) => {
-    console.log("connected to MongoDB");
-  
-    const asset = new Asset({
+    const asset = {
       // random number between 1 and 1000
       id: Math.floor(Math.random() * (1000 - 1 + 1)) + 1,
       asset: `${body.asset}`,
       amount: `${body.amount}`,
     }
-    )
-  
-    return asset.save();
-  }).then(() => {
-    console.log("asset saved!");
-    return mongoose.connection.close();
-  })
+    assets.push(asset);
+    console.log(assets);
   response.json("ok");
+});
+
+app.get("/api/assets", (request, response) => {
+  response.json(JSON.stringify(assets));
 });
 
 const PORT = process.env.PORT || "8080";
